c95f6fe9755e6fa1ff4d7a163eea7307d55d31a1
qiyuan <1184863960@qq.com>
2025-02-28 14:28:07 +0800

    feat: 活动信息API迭代和功能完善

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -5,8 +5,20 @@ const apiHostUrl = 'http://localhost:8091'
  * 装配抽奖
  * @param strategyId
  */
-export const strategyArmory = (strategyId?: number) => {
-    return fetch(`${apiHostUrl}/api/v1/raffle/strategy_armory?strategyId=${strategyId}`, {
+// export const strategyArmory = (strategyId?: number) => {
+//     return fetch(`${apiHostUrl}/api/v1/raffle/strategy_armory?strategyId=${strategyId}`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+// }
+/**
+ * 装配抽奖
+ * @param activityId
+ */
+export const activityStrategyArmory = (activityId?: number) => {
+    return fetch(`${apiHostUrl}/api/v1/raffle/activity/armory?activityId=${activityId}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -18,15 +30,41 @@ export const strategyArmory = (strategyId?: number) => {
  * 查询抽奖奖品列表
  * @param strategyId 策略ID
  */
-export const queryRaffleAwardList = (strategyId?: number) => {
+// export const queryRaffleAwardList = (strategyId?: number) => {
+//     try {
+//         return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 strategyId: strategyId
+//             })
+//         });
+//     } catch (error) {
+//         return fetch("{\n" +
+//             "    \"code\": \"0001\",\n" +
+//             "    \"info\": \"调用失败\",\n" +
+//             "    \"data\": [\n" +
+//             "}");
+//     }
+// }
+
+/**
+ * 查询抽奖奖品列表
+ * @param userId 用户ID
+ * @param activityId 用户ID
+ */
+export const queryRaffleAwardList = (userId?: string, activityId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list`, {
+        return fetch(`${apiHostUrl}/api/v1/raffle/strategy/query_raffle_award_list`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                strategyId: strategyId
+                userId: userId,
+                activityId: activityId
             })
         });
     } catch (error) {
@@ -37,6 +75,7 @@ export const queryRaffleAwardList = (strategyId?: number) => {
             "}");
     }
 }
+
 
 /**
  * 随机抽奖接口
@@ -52,15 +91,42 @@ export const queryRaffleAwardList = (strategyId?: number) => {
  * 	}
  * }
  */
-export const randomRaffle = (strategyId?: number) => {
+// export const randomRaffle = (strategyId?: number) => {
+//     try {
+//         return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json;charset=utf-8'
+//             },
+//             body: JSON.stringify({
+//                 strategyId: strategyId
+//             })
+//         })
+//     } catch (error) {
+//         return fetch("{\n" +
+//             "    \"code\": \"0001\",\n" +
+//             "    \"info\": \"调用失败\",\n" +
+//             "    \"data\": [\n" +
+//             "}");
+//     }
+
+
+
+/**
+ * 抽奖接口
+ * @param userId 用户ID
+ * @param activityId 用户ID
+ */
+export const draw = (userId?: string, activityId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle`, {
+        return fetch(`${apiHostUrl}/api/v1/raffle/activity/draw`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
             body: JSON.stringify({
-                strategyId: strategyId
+                userId: userId,
+                activityId: activityId
             })
         })
     } catch (error) {
--- src/app/components/StrategyArmory.tsx
+++ src/app/components/StrategyArmory.tsx
@@ -1,17 +1,47 @@
-import {strategyArmory} from "@/apis";
+// import {strategyArmory} from "@/apis";
+//
+// export function StrategyArmory() {
+//     const strategyArmoryHandle = async () => {
+//         const queryParams = new URLSearchParams(window.location.search);
+//         const strategyId = Number(queryParams.get('strategyId'));
+//         if (!strategyId){
+//             window.alert("请在请求地址中，配置 strategyId 值，如：http://localhost:3000/?strategyId=100006")
+//             return;
+//         }
+//         const res = await strategyArmory(strategyId);
+//         const {code, info} = await res.json();
+//         if (code != "0000") {
+//             window.alert("抽奖策略装配失败 code:" + code + " info:" + info)
+//             return;
+//         }
+//     }
+//
+//     return (
+//         <div
+//             className="px-6 py-2 mb-8 text-white bg-blue-500 rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+//             style={{cursor: "pointer"}}
+//             onClick={strategyArmoryHandle}
+//         >
+//             装配抽奖
+//         </div>
+//     );
+// }
+
+
+import {activityStrategyArmory} from "@/apis";
 
 export function StrategyArmory() {
     const strategyArmoryHandle = async () => {
         const queryParams = new URLSearchParams(window.location.search);
-        const strategyId = Number(queryParams.get('strategyId'));
-        if (!strategyId){
-            window.alert("请在请求地址中，配置 strategyId 值，如：http://localhost:3000/?strategyId=100006")
+        const activityId = Number(queryParams.get('activityId'));
+        if (!activityId){
+            window.alert("请在请求地址中，配置 activityId 值，如：http://localhost:3000/?activityId=100301")
             return;
         }
-        const res = await strategyArmory(strategyId);
+        const res = await activityStrategyArmory(activityId);
         const {code, info} = await res.json();
         if (code != "0000") {
-            window.alert("抽奖策略装配失败 code:" + code + " info:" + info)
+            window.alert("抽奖活动策略装配失败 code:" + code + " info:" + info)
             return;
         }
     }
